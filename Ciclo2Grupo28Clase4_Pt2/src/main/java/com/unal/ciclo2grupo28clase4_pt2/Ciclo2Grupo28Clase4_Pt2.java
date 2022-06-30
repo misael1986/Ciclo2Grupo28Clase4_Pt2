fd7227cdc72aa913f52e0963035dfe1358ef5790
misael1986 <gabelogan0167@hotmail.com>
2022-06-29 22:05:18 -0500

    Add files via upload
    Actualización

--- Ciclo2Grupo28Clase4_Pt2/src/main/java/com/unal/ciclo2grupo28clase4_pt2/Ciclo2Grupo28Clase4_Pt2.java
+++ Ciclo2Grupo28Clase4_Pt2/src/main/java/com/unal/ciclo2grupo28clase4_pt2/Ciclo2Grupo28Clase4_Pt2.java
@@ -17,13 +17,22 @@ public class Ciclo2Grupo28Clase4_Pt2 {
         System.out.println("es minuscula?: " + esMinuscula(letra));
         System.out.println("es Par?: " + esASCIIPar(letra));
          */
-        /*System.out.println("Digite un numero real:");
+ /*System.out.println("Digite un numero real:");
         double num = te.nextDouble();
         System.out.println(determinarNumero(num));
+         */
+        //System.out.println(determinarPunto(0, 2.8));
+       
+        /*
+        System.out.println(ejesPlano(1.5, 6));
+        System.out.println(ejesPlano(-3, 5.5));
+        System.out.println(ejesPlano(0, 10.0001));
+        System.out.println(ejesPlano(-2, -3));
+        System.out.println(ejesPlano(8, -5));
         */
         
-        System.out.println(determinarPunto(0,2.8));
-        
+        System.out.println(desigualdadTriangular(0.5 ,4.4 ,5));
+
     }
 
     //--------------
@@ -64,13 +73,54 @@ public class Ciclo2Grupo28Clase4_Pt2 {
         x = 0;
         y = 0;
         r = 3;
-        double p1=Math.pow(x-h,2);
-        double p2=Math.pow(y-k,2);
-        if ((p1+p2) <= (r * r)) {
+        double p1 = Math.pow(x - h, 2);
+        double p2 = Math.pow(y - k, 2);
+        if ((p1 + p2) <= (r * r)) {
             siEsta = true;
         }
 
         return siEsta;
+    }
+
+    //----EJES DEL PLANO CARTESIANO
+    public static String ejesPlano(double x, double y) {
+        String respuesta = "";
+        if (x == 0 || y == 0) {
+            respuesta = "Ejes";
+        } else if (x > 0 && y > 0) {
+            respuesta = "I";
+        } else if (x < 0 && y > 0) {
+            respuesta = "II";
+        } else if (x < 0 && y < 0) {
+            respuesta = "III";
+        } else {
+            respuesta = "IV";
+        }
+
+        return respuesta;
+    }
+
+    //---Desigualdad triangular
+    public static boolean desigualdadTriangular(double a, double b, double c) {
+        boolean siCumplen = false;
+        boolean banderaA = false;
+        boolean banderaB = false;
+        boolean banderaC = false;
+
+        if (a < (b + c)) {
+            banderaA = true;
+        }
+        if (b < (a + c)) {
+            banderaB = true;
+        }
+        if (c < (a + b)) {
+            banderaC = true;
+        }
+
+        if (banderaA == true && banderaB == true && banderaC == true) {
+            siCumplen = true;
+        }
+        return siCumplen;
     }
 
 }
